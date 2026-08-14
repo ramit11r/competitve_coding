6beacfa4b0bdef737cea38be73f9996f3899413d
ramitkoirala <ramitkoirala@gmail.com>
2026-08-14 19:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (23.16%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        int n=sqrt(c);
-        vector<int> a;
-        for(int i=0;i<=n;i++){
-            a.push_back(i);
-        }
-        int i=0,j=n;
+        int i=0,j=sqrt(c);
         while(i<=j){
-            if((pow(a[i],2)+pow(a[j],2))<c){
+            if((pow(i,2)+pow(j,2))<c){
                 i++;
             }
-            else if(pow(a[i],2)+pow(a[j],2)==c){
+            else if(pow(i,2)+pow(j,2)==c){
                 return true;
             }
             else{
