414e4c7fde31e410ebcc17d3fab0b65a88be27f0
ramitkoirala <ramitkoirala@gmail.com>
2026-08-25 18:33:12 +0530

    Time: 6 ms (12.42%) | Memory: 25 MB (53.82%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -3,11 +3,8 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
         int n=nums.size();
         int a=1;
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        while(mp[k*a]!=0){
+        unordered_set<int> st(nums.begin(),nums.end());
+        while(st.count(k*a)){
             a++;
         }
         return k*(a);
