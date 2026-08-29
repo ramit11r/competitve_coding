c68be604492d3804cacd4c0e9b55af078e3e16e0
ramitkoirala <ramitkoirala@gmail.com>
2026-08-29 18:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 29.8 MB (22.80%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -4,7 +4,7 @@ public:
         int ans=INT_MAX;
         int n=words.size();
         for(int i=0;i<words.size();i++){
-            if(words[i%words.size()]==target){
+            if(words[i]==target){
                 int dist=min(abs(i-startIndex),n-abs(i-startIndex));
                 ans=min(ans,dist);
             } 
