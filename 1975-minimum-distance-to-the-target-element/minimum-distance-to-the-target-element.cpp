a33272d9b32b3ba969c1f400700351926c84f6fa
ramitkoirala <ramitkoirala@gmail.com>
2026-08-29 17:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (72.61%) - LeetSync

--- 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.cpp
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int getMinDistance(vector<int>& nums, int target, int start) {
-        int n=nums.size();
         int ans=INT_MAX;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
             if(nums[i]==target){
                 ans=min(ans,abs(i-start));
             }
