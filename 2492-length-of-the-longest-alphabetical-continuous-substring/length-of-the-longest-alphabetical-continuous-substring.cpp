b97a671c9542934f8e26f2eb9ad2426a51953b38
ramitkoirala <ramitkoirala@gmail.com>
2026-09-04 18:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (88.00%) - LeetSync

--- 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -6,12 +6,10 @@ public:
         int ans=1;
         for(int j=1;j<s.size();j++){
             if(s[j]-s[j-1]==1){
-                size=j-i+1;
+                ans=max(ans,j-i+1);
             }
-            ans=max(ans,size);
             if(s[j]-s[j-1]!=1){
                 i=j;
-                size=0;
             }
         }
         return ans;
