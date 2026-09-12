77e9a6621923513a2e90939319442eb200d66777
ramitkoirala <ramitkoirala@gmail.com>
2026-09-12 18:37:40 +0530

    Time: 23 ms (5.51%) | Memory: 23.1 MB (24.30%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        if(h==n){
+            return *max_element(piles.begin(),piles.end()); 
+        }
+        int tothr=0;
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        while(low<high){
+            int k=low+(high-low)/2;
+            tothr=0;
+            for(int i=0;i<n;i++){
+                tothr+=ceil((double)piles[i]/k);
+            }
+            if(tothr<=h){
+                high=k;
+            }
+            else{
+                low=k+1;
+            }
+        }
+        return high;
+    }
+};
