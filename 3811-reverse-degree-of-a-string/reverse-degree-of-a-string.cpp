16fce585327ca360cea74488d256c815f7144224
ramitkoirala <ramitkoirala@gmail.com>
2026-09-20 17:26:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (78.39%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -3,8 +3,7 @@ public:
     int reverseDegree(string s) {
         int sum=0;
         for(int i=0;i<s.size();i++){
-            int pro=(26-(s[i]-'a'))*(i+1);
-            sum+=pro;
+            sum+=(26-(s[i]-'a'))*(i+1);
         }
         return sum;
     }
