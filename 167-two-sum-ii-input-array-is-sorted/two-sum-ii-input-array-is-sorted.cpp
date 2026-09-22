56d8ba662f7f9e77501771a985ed379d8c95a54c
ramitkoirala <ramitkoirala@gmail.com>
2026-09-22 11:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (6.03%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -4,10 +4,10 @@ public:
         int i=0,j=numbers.size()-1;
         while(i<j){
             if(numbers[i]+numbers[j]==target) return {i+1,j+1};
-            while(numbers[i]+numbers[j]<target){
+            if(numbers[i]+numbers[j]<target){
                 i++;
             }
-            while(numbers[i]+numbers[j]>target){
+            if(numbers[i]+numbers[j]>target){
                 j--;
             }
         }
